bd98542dd8e624dcea25c45c81ab8ec706e41ab2
ashen114 <1215772764@qq.com>
2019-06-28 10:59:00 +0800

    add 上帝视角

--- Assets/script/CameraMove.cs
+++ Assets/script/CameraMove.cs
@@ -3,19 +3,94 @@ using System.Collections;
 
 public class CameraMove : MonoBehaviour
 {
-  /// 镜头偏移量
+  /// 摄像机对于跟随对象的偏移量
   private Vector3 offset;
-  // 跟随对象
+  // 跟随对象-改用private，采用查找标签的方式寻找跟随对象
   public Transform player;
-  private void Start()
+
+  // 距离
+  private float distance;
+
+  // 5个点作为摄像机可选位置
+  private Vector3[] points = new Vector3[5];
+
+  // 筛选后的摄像机位置
+  private Vector3 targetPos;
+
+  private void Awake()
   {
-    offset = player.position - transform.position;
+    // player = GameObject.FindWithTag("Player").transform;
+    offset = transform.position - player.position;
+    distance = offset.magnitude;
   }
 
-  private void Update()
+  private void FixedUpdate()
   {
-    transform.position = Vector3.Lerp(transform.position, player.position - offset, Time.deltaTime * 5);
-    Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
-    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 3f);
+    // 更新5个点的位置
+    points[0] = player.position + offset;
+    points[4] = player.position + Vector3.up * distance;
+
+    points[1] = Vector3.Lerp(points[0], points[4], 0.25f);
+    points[2] = Vector3.Lerp(points[0], points[4], 0.5f);
+    points[3] = Vector3.Lerp(points[0], points[4], 0.75f);
+    points[4] = Vector3.Lerp(points[0], points[4], 0.9f);
+
+    targetPos = FindCameraTarget();
+
+    AdjustCamera();
   }
+
+  private Vector3 FindCameraTarget()
+  {
+    // 头顶位置
+    Vector3 result = points[points.Length - 1];
+
+    // 从低到高遍历
+    for (int i = 0; i < points.Length; ++i)
+    {
+      // 如果挡住了player
+      if (IsHitPlayer(points[i], player.position))
+      {
+        result = points[i];
+        break;
+      }
+    }
+    return result;
+  }
+
+  private Ray ray;
+  private RaycastHit hit;
+  /// <summary> 检查跟随对象是否被物体遮挡
+  /// 从origin发射射线检测是否触碰到跟随对象
+  /// 碰到则摄像机位置在此可看到跟随对象
+  /// </summary>
+  /// <param name="origin">发射源</param>
+  /// <param name="target">目标对象</param>
+  /// <returns></returns>
+  bool IsHitPlayer(Vector3 origin, Vector3 target)
+  {
+    bool result = false;
+
+    Vector3 dir = target - origin;
+    ray = new Ray(origin, dir);
+    if (Physics.Raycast(ray, out hit))
+    {
+      if (hit.transform.tag == "Player")
+      {
+        result = true;
+      }
+    }
+    return result;
+  }
+
+  /// <summary> 调整摄像机位置
+  /// </summary>
+  private void AdjustCamera()
+  {
+    transform.position = Vector3.Slerp(transform.position, targetPos, Time.deltaTime * 6);
+
+    Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
+    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 33f);
+  }
+
 }
--- Assets/script/test.cs
+++ Assets/script/test.cs
@@ -1,23 +1,92 @@
 using UnityEngine;
 using System.Collections;
 
-public class CamFollow1 : MonoBehaviour
+public class CamFollow3 : MonoBehaviour
 {
 
-  private Vector3 offset;
-  public Transform player;
 
-  void Start()
+  private Transform player;//英雄位置
+  private Vector3 offset; //摄像机和英雄相对位置
+  private float distance;//距离
+  private Vector3[] points = new Vector3[5];//5个点作为摄像机位置的选择
+
+  private Vector3 targetPos;//筛选后的摄像机位置
+
+  void Awake()
   {
-    offset = player.position - transform.position;
+    player = GameObject.FindWithTag("Player").transform;
+    offset = transform.position - player.position;
+    distance = offset.magnitude;
   }
 
-  void Update()
+  void FixedUpdate()
   {
-    transform.position = Vector3.Lerp(transform.position, player.position - offset, Time.deltaTime * 5);
-    // 增加注视效果
+    //更新5个点的位置
+    points[0] = player.position + offset;
+    points[4] = player.position + Vector3.up * distance;
+
+    points[1] = Vector3.Lerp(points[0], points[4], 0.25f);
+    points[2] = Vector3.Lerp(points[0], points[4], 0.5f);
+    points[3] = Vector3.Lerp(points[0], points[4], 0.75f);
+    points[4] = Vector3.Lerp(points[0], points[4], 0.9f);
+
+    targetPos = FindCameraTarget();
+
+    AdjustCamera();
+  }
+
+  private Vector3 FindCameraTarget()
+  {
+    Vector3 result = points[points.Length - 1];//头顶位置
+
+    //从低到高遍历
+    for (int i = 0; i < points.Length; ++i)
+    {
+      if (IsHitPlayer(points[i], player.position))
+      {
+        result = points[i];
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  private Ray ray;
+  private RaycastHit hit;
+  /// <summary>
+  /// 从origin发射一条射线检测是否碰到player，
+  /// 碰到则表示摄像机在此位置可以看到player
+  /// </summary>
+  /// <param name="origin"></param>
+  /// <param name="target"></param>
+  /// <returns></returns>
+  bool IsHitPlayer(Vector3 origin, Vector3 target)
+  {
+    bool result = false;
+
+    Vector3 dir = target - origin;
+    ray = new Ray(origin, dir);
+    if (Physics.Raycast(ray, out hit))
+    {
+      if (hit.transform.tag == "Player")
+      {
+        result = true;
+      }
+    }
+    return result;
+  }
+
+  /// <summary>
+  /// 调整摄像机位置
+  /// </summary>
+  void AdjustCamera()
+  {
+
+    transform.position = Vector3.Slerp(transform.position, targetPos, Time.deltaTime * 6);
+
     Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
-    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 3f);
+    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 33f);
 
   }
 }
